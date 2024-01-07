395c0763bd5005cf7311d4bc088f7b6827ffdbd5
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-01-07 14:02:12 +0100

    WIP

---
+++ src/Controls/samples/Controls.Sample.UITests/Issues/Issue19733.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace Maui.Controls.Sample.Issues
+{
+	[Issue(IssueTracker.Github, 19733, "No unintended focus change occurs", PlatformAffected.iOS)]
+	public partial class Issue19733 : ContentPage
+	{
+		public Issue19733()
+		{
+			InitializeComponent();
+		}
+
+		//private void OnTapped(object sender, TappedEventArgs e)
+		//{
+		//	// Report that the callback was called.
+		//	layout.Children.Add(new Label { Text = "TapAccepted", AutomationId = "TapAccepted" });
+
+		//	string result = "Failure";
+		//	string automationId = "Failure";
+
+		//		//result = "Error: position is null";
+		//		//result = $"Success: relative position is: X={position.Value.X}, Y={position.Value.Y}";
+		//		// automationId = "Success";
+
+		//	// Report the result of the test.
+		//	layout.Children.Add(new Label { Text = result, AutomationId = automationId });
+		//}
+	}
+}
---
+++ src/Controls/tests/UITests/Tests/Issues/Issue19733.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+using System.Drawing;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue19733 : _IssuesUITest
+	{
+		public Issue19733(TestDevice device) : base(device) { }
+
+		public override string Issue => "No unintended focus change occurs";
+
+		[Test]
+		public async Task NoUnintendedFocusShouldOccur()
+		{
+			_ = App.WaitForElement("Entry3");
+
+			App.Click("Entry3");
+			App.Click("Label3");
+
+			// Wait if there is a focus change or not.
+			await Task.Delay(2000);
+
+			App.WaitForElement("Success");
+		}
+	}
+}

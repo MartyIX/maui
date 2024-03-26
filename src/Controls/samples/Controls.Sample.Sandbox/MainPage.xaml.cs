a6363a6910ebd098a399e593bc253b0256a1b992
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-03-26 20:16:49 +0100

    New demo

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -15,9 +15,40 @@ namespace Maui.Controls.Sample
 			InitializeComponent();
 		}
 
-		private void DragGestureRecognizer_DragStarting(object sender, DragStartingEventArgs e)
+		private void DragGestureRecognizer_DragStarting(object? sender, DragStartingEventArgs e)
 		{
 
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			for (int i = 0; i < 2000; i++)
+			{
+				// Add the following XAML.
+				/*
+					<Rectangle Stroke="Red" Fill="DarkBlue" StrokeThickness="4" HeightRequest="200" WidthRequest="200">
+						<Rectangle.GestureRecognizers>
+							<DragGestureRecognizer DragStarting="DragGestureRecognizer_DragStarting" />
+						</Rectangle.GestureRecognizers>
+					</Rectangle>
+				 */
+
+				Microsoft.Maui.Controls.Shapes.Rectangle rectangle = new()
+				{
+					Fill = Colors.DarkBlue,
+					Stroke = Colors.Red,
+					StrokeThickness = 4,
+					WidthRequest = 200,
+					HeightRequest = 200,
+				};
+
+				DragGestureRecognizer recognizer = new();
+				recognizer.DragStarting += DragGestureRecognizer_DragStarting;
+
+				rectangle.GestureRecognizers.Add(recognizer);
+
+				myLayout.Add(rectangle);
+			}
 		}
 	}
 }

f7643d6d57364a691ab2c989258df305be9fb439
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-04-11 21:45:08 +0200

    WIP

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -1,18 +1,50 @@
 ﻿using System;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Maui;
+using System.ComponentModel;
 using Microsoft.Maui.Controls;
-using Microsoft.Maui.Graphics;
 
 namespace Maui.Controls.Sample
 {
 	public partial class MainPage : ContentPage
 	{
+		private bool isEntryVisible;
+
+		public bool IsEntryVisible
+		{
+			get { return isEntryVisible; }
+			set {
+				if (isEntryVisible != value)
+				{
+					isEntryVisible = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
 		public MainPage()
 		{
+			BindingContext = this;
 			InitializeComponent();
+		}
+
+		private void SwitchVisibility(object sender, EventArgs e)
+		{
+			IsEntryVisible = !isEntryVisible;
+			myEntry.Focus();
+		}
+
+		private void SetFocus(object sender, EventArgs e)
+		{
+			myEntry.Focus();
+		}
+
+		private void myEntry_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			if (sender is not Entry entry)
+				throw new Exception($"Entry expected as a sender, '{sender?.GetType().FullName}' provided.");
+
+			if ((e.PropertyName == nameof(entry.IsVisible)) && entry.IsVisible)
+				// this.FocusVisualElement(entry);
+				myEntry.Focus();
 		}
 	}
 }

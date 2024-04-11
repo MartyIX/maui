840ebf5ab1bc95013c79c5d5e0743ae23af3c850
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-04-11 22:03:27 +0200

    Sandbox test

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -1,18 +1,44 @@
 ﻿using System;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Maui;
 using Microsoft.Maui.Controls;
-using Microsoft.Maui.Graphics;
 
-namespace Maui.Controls.Sample
+namespace Maui.Controls.Sample;
+
+public partial class MainPage : ContentPage
 {
-	public partial class MainPage : ContentPage
+	const int rowCount = 30;
+	const int columnCount = 30;
+
+	public MainPage()
 	{
-		public MainPage()
+		InitializeComponent();
+	}
+
+	private void ClearGrid_Clicked(object sender, EventArgs e)
+	{
+		Stopwatch sw = Stopwatch.StartNew();
+
+		myContentWrapper.Clear();
+
+		sw.Stop();
+
+		info.Text = $"Clearing all content took: {sw.ElapsedMilliseconds} ms";
+	}
+
+	private void BatchGenerate_Clicked(object sender, EventArgs e)
+	{
+		Stopwatch sw = Stopwatch.StartNew();
+
+		int batchSize = int.Parse(BatchSize.Text);
+		myContentWrapper.Clear();
+
+		for (int i = 0; i < batchSize; i++)
 		{
-			InitializeComponent();
+			Border border = new() { Stroke = Brush.AliceBlue, StrokeThickness = 2, Content = new Label() { Text = $"Test {i}" } };
+			myContentWrapper.Add(border);
 		}
+
+		sw.Stop();
+		info.Text = $"Created {batchSize} borders and it took {sw.ElapsedMilliseconds} ms in total. Avg run took {Math.Round(sw.ElapsedMilliseconds / (double)batchSize, 2)} ms";
 	}
 }

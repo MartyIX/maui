62dc732347de721cf4525af3ea33e8d46a54809c
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-07-11 20:11:00 +0200

    Test

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -5,5 +5,32 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+
+		PointerGestureRecognizer pointerGestureRecognizer = new();
+		pointerGestureRecognizer.PointerEnteredCommand = new Command(() =>
+		{
+			button3.Text = "Button 3 (pointer over)";
+			button3.BackgroundColor = Colors.Red;
+		});
+
+		pointerGestureRecognizer.PointerExitedCommand = new Command(() =>
+		{
+			button3.Text = "Button 3";
+			button3.BackgroundColor = null;
+		});
+
+		button3.GestureRecognizers.Add(pointerGestureRecognizer);
+	}
+
+	private void PointerGestureRecognizer_PointerEntered(object sender, PointerEventArgs e)
+	{
+		button2.Text = "Button 2 (pointer over)";
+		button2.BackgroundColor = Colors.Red;
+	}
+
+	private void PointerGestureRecognizer_PointerExited(object sender, PointerEventArgs e)
+	{
+		button2.Text = "Button 2";
+		button2.BackgroundColor = null;
 	}
 }

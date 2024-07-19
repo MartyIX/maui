1b8a4bc720d2df3eab046810c1e75283cb8fbe23
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-07-19 22:53:18 +0200

    test

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -1,9 +1,26 @@
-﻿namespace Maui.Controls.Sample;
+﻿using Microsoft.Maui.Graphics.Platform;
+
+namespace Maui.Controls.Sample;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		try
+		{
+			var imageUrl = "https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Surface-Laptop-AI-7Ed-Sapphire-MC001-3000x1682:VP5-1920x600";
+			var stream = await new HttpClient().GetStreamAsync(imageUrl);
+			var img = PlatformImage.FromStream(stream);
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex);
+		}
+
 	}
 }

468f77bce4109a24af9c2edd0ec8cdc1fabb3713
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-07-25 10:23:12 +0200

    Improve test

---
+++ src/Controls/samples/Controls.Sample.Sandbox/GraphicsDrawable.cs
@@ -0,0 +1,16 @@
+﻿namespace Maui.Controls.Sample;
+
+public class GraphicsDrawable(Microsoft.Maui.Graphics.IImage[] images) : IDrawable
+{
+	public void Draw(ICanvas canvas, RectF dirtyRect)
+	{
+		// 3x3 matrix composed of 100x100 images. The number of images is always 9. 
+		for (int i = 0; i < images.Length; i++)
+		{
+			int row = i / 3;
+			int column = i % 3;
+
+			canvas.DrawImage(images[i], column * 100, row * 100, 100, 100);
+		}		
+	}
+}
--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Maui.Graphics.Platform;
+﻿using System.IO;
+using Microsoft.Maui.Graphics.Platform;
 
 namespace Maui.Controls.Sample;
 
@@ -13,14 +14,32 @@ public partial class MainPage : ContentPage
 	{
 		try
 		{
-			var imageUrl = "https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Surface-Laptop-AI-7Ed-Sapphire-MC001-3000x1682:VP5-1920x600";
-			var stream = await new HttpClient().GetStreamAsync(imageUrl);
-			var img = PlatformImage.FromStream(stream);
+			using HttpClient httpClient = new();
+
+			int count = 9;
+			Stream[] streams = new Stream[count];
+			Task<Microsoft.Maui.Graphics.IImage>[] imageTasks = new Task<Microsoft.Maui.Graphics.IImage>[count];
+
+			// Download sample images.
+			for (int i = 0; i < count; i++)
+			{
+				streams[i] = await httpClient.GetStreamAsync("https://picsum.photos/100");
+			}
+
+			// Attempt to load them in parallel to make it more likely to hit any possible error.
+			for (int i = 0; i < count; i++)
+			{
+				Stream stream = streams[i];
+				imageTasks[i] = Task.Run(() => PlatformImage.FromStream(stream));
+			}
+
+			Microsoft.Maui.Graphics.IImage[] images = imageTasks.Select(task => task.Result).ToArray();
+
+			graphicsVIew.Drawable = new GraphicsDrawable(images);
 		}
 		catch (Exception ex)
 		{
 			Console.WriteLine(ex);
 		}
-
 	}
 }
